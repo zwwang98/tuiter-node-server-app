f081e78618dda9536f3a904f3c1920902ac1b450
ziweiwang98 <wang.ziwei2@northeastern.edu>
2023-03-22 19:24:25 -0700

    Finished '4 Interacting with a Web server using Postman'

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import HelloController from "./controllers/hello-controller.js";
 import UserController from "./controllers/users/users-controller.js";
 
 const app = express();
+app.use(express.json());
 
 HelloController(app);
 UserController(app);
--- controllers/users/users-controller.js
+++ controllers/users/users-controller.js
@@ -5,6 +5,9 @@ let users = people;
 const UserController = (app) => {
   app.get("/api/users", findUsers);
   app.get("/api/users/:uid", findUserById);
+  app.post("/api/users", createUser);
+  app.delete("/api/users/:uid", deleteUser);
+  app.put("/api/users/:uid", updateUser);
 };
 
 const findUserById = (req, res) => {
@@ -21,6 +24,28 @@ const findUsers = (req, res) => {
     return;
   }
   res.json(users);
+};
+
+const createUser = (req, res) => {
+  const newUser = req.body;
+  newUser._id = new Date().getTime() + "";
+  users.push(newUser);
+  res.json(newUser);
+};
+
+const deleteUser = (req, res) => {
+  const userId = req.params["uid"];
+  users = users.filter((usr) => usr._id !== userId);
+  res.sendStatus(200);
+};
+
+const updateUser = (req, res) => {
+  const userId = req.params["uid"];
+  const updates = req.body;
+  users = users.map((usr) =>
+    usr._id === userId ? { ...usr, ...updates } : usr
+  );
+  res.sendStatus(200);
 };
 
 export default UserController;
